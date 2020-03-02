db3ff1b6d78aca0ba0bfe093d7fe4f756c7745ff
SIDDHI <siddhi19patil@gmail.com>
2020-03-02 19:01:54 +0530

    card student view jobs

--- app/src/main/java/com/example/rem/ui_admin/viewfeedback_admin/ViewfeedbacksFragmentAdmin.java
+++ app/src/main/java/com/example/rem/ui_admin/viewfeedback_admin/ViewfeedbacksFragmentAdmin.java
@@ -11,11 +11,26 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.rem.Model.ViewFeedbackAdmin;
 import com.example.rem.R;
+import com.example.rem.ViewHolders.AdminFeedbackViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.firebase.ui.database.SnapshotParser;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ViewfeedbacksFragmentAdmin extends Fragment {
-
+    private RecyclerView viewFeedbackRecycler;
+    RecyclerView.LayoutManager layoutManager;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference rootRef,userRef;
     private ViewfeedbacksViewModelAdmin viewfeedbacksViewModelAdmin;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -23,13 +38,48 @@ public class ViewfeedbacksFragmentAdmin extends Fragment {
         viewfeedbacksViewModelAdmin =
                 ViewModelProviders.of(this).get(ViewfeedbacksViewModelAdmin.class);
         View root = inflater.inflate(R.layout.fragment_viewfeedback_admin, container, false);
-        final TextView textView = root.findViewById(R.id.text_send);
-        viewfeedbacksViewModelAdmin.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        firebaseAuth = FirebaseAuth.getInstance();
+        viewFeedbackRecycler = root.findViewById(R.id.recyclerView_viewfeedback_admin);
+        layoutManager =new LinearLayoutManager(getContext());
+        viewFeedbackRecycler.setLayoutManager(layoutManager);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        rootRef = firebaseDatabase.getReference();
+
         return root;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<ViewFeedbackAdmin> options = new FirebaseRecyclerOptions.Builder<ViewFeedbackAdmin>()
+                .setQuery(rootRef.child("student"), new SnapshotParser<ViewFeedbackAdmin>() {
+                    @NonNull
+                    @Override
+                    public ViewFeedbackAdmin parseSnapshot(@NonNull DataSnapshot snapshot) {
+                        return new ViewFeedbackAdmin(snapshot.child("profile").child("name").getValue().toString(),
+                                snapshot.child("feedbacks").child("feedback").getValue().toString()
+                                );
+                    }
+                })
+                .build();
+
+        FirebaseRecyclerAdapter<ViewFeedbackAdmin, AdminFeedbackViewHolder> adapter = new FirebaseRecyclerAdapter<ViewFeedbackAdmin, AdminFeedbackViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull AdminFeedbackViewHolder adminFeedbackViewHolder, int i, @NonNull ViewFeedbackAdmin viewFeedbackAdmin) {
+                adminFeedbackViewHolder.name.setText(viewFeedbackAdmin.getName());
+                adminFeedbackViewHolder.feedback.setText("Feedback : "+viewFeedbackAdmin.getFeedback());
+            }
+
+            @NonNull
+            @Override
+            public AdminFeedbackViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_viewfeedback_admin,parent,false);
+                AdminFeedbackViewHolder holder = new AdminFeedbackViewHolder(view);
+                return  holder;
+            }
+        };
+        viewFeedbackRecycler.setAdapter(adapter);
+        adapter.startListening();
     }
 }

f987c4494229c8c174bcc09b5a9e10a12bb2050a
SIDDHI <siddhi19patil@gmail.com>
2020-03-08 23:31:31 +0530

    Help added in all 3 modules

--- app/src/main/java/com/example/rem/ui_admin/help_admin/HelpFragmentAdmin.java
+++ app/src/main/java/com/example/rem/ui_admin/help_admin/HelpFragmentAdmin.java
@@ -28,6 +28,7 @@ public class HelpFragmentAdmin extends Fragment {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
+
             }
         });
         return root;
--- app/src/main/java/com/example/rem/ui_student/help_student/HelpViewModelStudent.java
+++ app/src/main/java/com/example/rem/ui_student/help_student/HelpViewModelStudent.java
@@ -10,7 +10,14 @@ public class HelpViewModelStudent extends ViewModel {
 
     public HelpViewModelStudent() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is student help fragment");
+        mText.setValue("Help for student\n" +
+                "1.Student can login to the page by providing id and password on different devices.\n" +
+                "2.Once you login as student , you did not need to login everytime in student unless you logout from module. \n" +
+                "3.After you login as student and the home page appears you can slide in from left side moving towards right or simply click on the toggle button in the upper left corner to get more options and navigate through other functionalities.\n" +
+                "4.The student has different functionalities like he/she can view company details, view notifications, search for the jobs that fit in his/her category ,see the available jobs and view the jobs that he/she applied for.\n" +
+                "5.The student has to first fill the profile and submit it. He/She will be able to see whether the information filled in the profile is correct or not in the home page and update the profile if anything is incorrect or in order to change something.\n" +
+                "6.The student is able to write and submit the feedbacks and also see the previous feedback of the user. \n" +
+                "7.If the student desires to go settings of the mobile he/she can directly navigate to the mobile settings by clicking the three dots at the top right corner instead of following the long procedure of closing app and starting the settings.\n");
     }
 
     public LiveData<String> getText() {

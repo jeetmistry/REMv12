0d770362eb134968cf84b7f5c239c066b47cb773
SIDDHI <siddhi19patil@gmail.com>
2020-03-16 20:37:37 +0530

    About us updated Developers added

--- app/src/main/java/com/example/rem/AboutUs.java
+++ app/src/main/java/com/example/rem/AboutUs.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class AboutUs extends AppCompatActivity {
 
@@ -13,6 +16,8 @@ public class AboutUs extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
         TextView textView=(TextView)findViewById(R.id.abouttxt);
+
+
 
 
     }
